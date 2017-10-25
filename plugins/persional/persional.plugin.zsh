5e1e0740a875dd43beb28f3f4b564021a7bf0092
Nguyen Hoai Nam <namptit307@gmail.com>
2017-10-26 04:44:49 +0700

    Update alias

--- plugins/persional/persional.plugin.zsh
+++ plugins/persional/persional.plugin.zsh
@@ -2,3 +2,4 @@ alias md='mkdir'
 alias rmd='rm -rf'
 alias filetree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'"
 alias grw='git review'
+alias ls='ls --hide="*.pyc"'
