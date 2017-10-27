b7c08327a516f58ffe513fdf849558846d8d03d2
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-10-27 08:55:51 +0700

    Update configuration

--- plugins/persional/persional.plugin.zsh
+++ plugins/persional/persional.plugin.zsh
@@ -1,5 +1,17 @@
+# For alias
 alias md='mkdir'
 alias rmd='rm -rf'
 alias filetree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'"
-alias grw='git review'
 alias ls='ls --hide="*.pyc"'
+
+if [ -d ~/MyGit ] ; then
+    alias cdg='cd ~/MyGit'
+fi
+
+if [ -f ~/.get_info_review.py ] ; then
+   alias ncm='python ~/.get_info_review.py'
+fi
+
+# Git
+alias grw='git review'
+
